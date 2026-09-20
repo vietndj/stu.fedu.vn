97a84d8eb856bae53df3dbfead883a5490111b53
Nguyễn Việt <vietndj@gmail.com>
2026-09-21 00:49:29 +0700

    chore: remove temp scripts

--- fix_delete.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('hocvien.html', 'utf8');
-
-html = html.replace(
-  '      currentStudents = currentStudents.filter(s => s.id !== deleteTargetId);\n      saveStudents(id);',
-  '      currentStudents = currentStudents.filter(s => s.id !== deleteTargetId);\n      saveStudents(deleteTargetId);'
-);
-
-fs.writeFileSync('hocvien.html', html);
--- fix_syntax.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('hocvien.html', 'utf8');
-
-// The first replacement made it `const INITIAL_STUDENTS = [];`
-// We need to change that to `let INITIAL_STUDENTS = [];` and remove the second declaration.
-html = html.replace('const INITIAL_STUDENTS = [];', '');
-
-fs.writeFileSync('hocvien.html', html);
-console.log("Fixed SyntaxError");
